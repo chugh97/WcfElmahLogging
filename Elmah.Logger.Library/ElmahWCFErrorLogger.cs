70803ffc1eb4cd29ccad9fd15be1d8bde7d64a8e
chugh97 <chugh97@yahoo.com>
2014-05-14 22:59:03 +0100

    WCF with Elmah integration

---
+++ Elmah.Logger.Library/ElmahWCFErrorLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+
+namespace Elmah.Logger.Library
+{
+    
+    public class ElmahWCFErrorLogger : IErrorHandler
+    {
+        public bool HandleError(Exception error)
+        {
+            return false;
+        }
+
+        public void ProvideFault(Exception error, System.ServiceModel.Channels.MessageVersion version, ref System.ServiceModel.Channels.Message fault)
+        {
+            if (error == null)
+                return;
+            ///In case we run outside of IIS, 
+            ///make sure aspNetCompatibilityEnabled="true" in web.config under system.serviceModel/serviceHostingEnvironment
+            ///to be sure that HttpContext.Current is not null
+            if (HttpContext.Current == null)
+                return;
+            Elmah.ErrorSignal.FromCurrentContext().Raise(error);
+        }
+    }
+    
+}
---
+++ ElmahTestClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElmahTestClient.client;
+
+namespace ElmahTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = new Service1Client();
+            try
+            {
+                proxy.GetData(4);
+            }
+            catch (Exception ex){
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                proxy.Abort();
+            }
+            Console.Read();
+            
+        }
+    }
+}
